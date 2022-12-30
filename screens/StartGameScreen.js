a944be2a3aede349d0acb35c156dcad04f8d9e0b
Allen Mordukhaev <allenmor93@gmail.com>
2022-12-30 11:30:15 -0500

    init

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native'
+import React from 'react'
+
+export default function StartGameScreen() {
+  return (
+    <View>
+      <Text>StartGameScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
